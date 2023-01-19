dad6560450fff60a9f9545210267471d5a8282a5
Guedesleo <leonardo.guedes.rocha@gmail.com>
2023-01-18 22:35:35 -0300

    Revert "refactor: connect to mongodb before integration tests"
    This reverts commit d95a56de39c928258ca2d9b82128f61250014a31.

--- src/main/config/env.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  mongoUrl: process.env.MONGO_URL,
-  port: process.env.PORT || 3000,
-};
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,3 @@
-import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
-import env from "./config/env";
+import app from "./config/app";
 
-MongoHelper.connect(env.mongoUrl)
-  .then(async () => {
-    const app = (await import("./config/app")).default;
-    app.listen(env.port, () =>
-      console.log(`Server running at http://localhsot:${env.port}`)
-    );
-  })
-  .catch(console.error);
+app.listen(3000, () => console.log("Server running at http://localhsot:3000"));
