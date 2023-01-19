0dbd1a0e53d27a75f46a811fac25adaaedb466cf
Guedesleo <leonardo.guedes.rocha@gmail.com>
2023-01-18 22:20:28 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,16 @@
+import app from "../config/app";
+import request from "supertest";
+
+describe("Signup Routes", () => {
+  test("Should return an account on success", async () => {
+    await request(app)
+      .post("/api/signup")
+      .send({
+        name: "Leonardo",
+        email: "leonardo.guedes@gmail.com",
+        password: "123",
+        passwordConfirmation: "123",
+      })
+      .expect(200);
+  });
+});
