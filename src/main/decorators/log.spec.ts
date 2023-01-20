2bea051444b9653dee2f6038e4235c56d34ac0b0
Guedesleo <leonardo.guedes.rocha@gmail.com>
2023-01-19 21:30:48 -0300

    refactor: move sut creation to a facotry method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  HttpReponse,
+  HttRequest,
+} from "../../presentation/protocols";
+import { LogControllerDecorator } from "./Log";
+
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttRequest): Promise<HttpReponse> {
+      const httpResponse: HttpReponse = {
+        statusCode: 200,
+        body: {
+          name: "Leoanrdo",
+        },
+      };
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
+describe("LogController Decorator", () => {
+  test("Should call controller handle", async () => {
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, "handle");
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        name: "any_name",
+        passowrd: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
