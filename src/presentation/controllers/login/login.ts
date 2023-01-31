b3a6a201f70b4a9e363c5b28043dc5a863072f21
Guedesleo <leonardo.guedes.rocha@gmail.com>
2023-01-31 20:27:35 -0300

    feat: ensure LoginController call EmialValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,15 +1,25 @@
 import { MissingParmError } from "../../errors";
 import { badRequest } from "../../helpers/http-helper";
 import { Controller, HttpReponse, HttRequest } from "../../protocols";
+import { EmailValidator } from "../signup/signup-protocols";
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
   async handle(httpRequest: HttRequest): Promise<HttpReponse> {
     if (!httpRequest.body.email) {
       return new Promise((resolve) =>
         resolve(badRequest(new MissingParmError("email")))
       );
     }
-    return new Promise((resolve) =>
-      resolve(badRequest(new MissingParmError("password")))
-    );
+
+    if (!httpRequest.body.password) {
+      return new Promise((resolve) =>
+        resolve(badRequest(new MissingParmError("password")))
+      );
+    }
+
+    this.emailValidator.isValid(httpRequest.body.email);
   }
 }
