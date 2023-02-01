d42dadee04f967271de2647e140ecf8f2af78dc4
Guedesleo <leonardo.guedes.rocha@gmail.com>
2023-01-31 21:39:01 -0300

    refactor: move required field validaiton to the ValidaitonComposite

---
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -0,0 +1,16 @@
+import { Validation } from "./validation";
+
+export class ValidationComposite implements Validation {
+  private readonly validaitons: Validation[];
+  constructor(validaitons: Validation[]) {
+    this.validaitons = validaitons;
+  }
+  validate(input: any): Error {
+    for (const validation of this.validaitons) {
+      const error = validation.validate(input);
+      if (error) {
+        return error;
+      }
+    }
+  }
+}
